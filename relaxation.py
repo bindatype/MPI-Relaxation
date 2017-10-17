38a301cdb19ce421a0320a240f0e2e5a515a945c
Glen Alan MacLachlan <maclach@gwu.edu>
2017-10-17 18:20:23 -0400

    third commit to add subROWS

--- relaxation.py
+++ relaxation.py
@@ -13,16 +13,22 @@ ROWS = 8
 if size > ROWS:
 	print("Not enough ROWS")
 	exit()
+subROWS=ROWS//size+2
+print subROWS
+
 if rank == 0:
-	M=numpy.array(range(COLS*ROWS)).reshape((ROWS, COLS))
+	M=numpy.array(range(COLS*(ROWS+2))).reshape((ROWS+2, COLS))
 #	M=numpy.empty((ROWS+2, COLS))
 	M[0,:] = 1
 	M[:,0] = 1
-	for i in M: 
-   		print i
+	for elem in M: 
+   		print elem
 
 	# Init Grid
 
-#if rank ==0:
-#	for i in M[:,0]:
-#		print i
+if rank ==0:
+	for elem in M[0:4,:]:
+		print elem
+if rank ==0:
+        for elem in M[6:10,:]:
+                print "+",elem
