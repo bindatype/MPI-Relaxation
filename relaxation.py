f7759c625e8336b706d25235425347e73bd20d44
Glen Alan MacLachlan <maclach@gwu.edu>
2017-10-18 11:36:15 -0400

    Working out indexing scheme to push edges back and forth to neighboring ranks.

--- relaxation.py
+++ relaxation.py
@@ -24,35 +24,11 @@ if rank == 0:
 	M[0,:] = 1.
 	M[:,0] = 1.
 
+#distribute initial grid
 M=comm.bcast(M,root=0)
 comm.barrier()
-#for elem in M: 
-#	print elem
 
-#if rank ==0:
-#	for elem in M[0:4,:]: # rank 0
-#		print "-",elem
-#	for elem in M[2:6,:]: # rank 1 
-#		print "*",elem
-#	for elem in M[4:8,:]: # rank 2
-#		print "%",elem
-#        for elem in M[6:10,:]: # rank 3
-#                print "+",elem
-
-	# M[2*rank:2*rank+subROWS]
-
-#distribute initial grid
 subM = None
-#if rank == 0:
-#	for destination in xrange(size):
-#		subM = M[2*destination:2*destination+subROWS,:]
-#		comm.send(subM,dest=destination)
-
-#subM = comm.recv(source=0,status = stat)
-#sender = stat.Get_source()
-#for elem in subM:
-#print "Rank: %d\tSender: %d\tMessage:\n %s" % (rank,sender,subM)
-
 
 sender = -5
 subM = M[2*rank:2*rank+subROWS,:]
@@ -63,15 +39,20 @@ for elem in xrange(1,COLS-1):
 #print "New Rank: %d\tSender: %d\tMessage:\n %s" % (rank,sender,subM)
 M[2*rank:2*rank+subROWS,:]=subM 
 
-if rank == 0:
-	print "Rank 0"
-	for elem in M:
-		print elem	
-elif rank == size-1:
-	print "Rank size-1"
-else: 
-	print "Else"
 
+if rank == 0:
+	comm.send(M[subROWS-2,:],dest=rank+1)
+	M[subROWS-1,:]=comm.recv(source=rank+1)
+elif rank == size-1:
+	comm.send(M[2*rank+1,:],dest=rank-1)
+	M[ROWS-subROWS,:]=comm.recv(source=rank-1)
+else: 
+	comm.send(M[rank*subROWS,:],dest=rank-1)
+	comm.send(M[(rank+0)*subROWS,:],dest=rank+1)
+	M[ROWS-subROWS,:]=comm.recv(source=rank-1)
+	M[2*rank+subROWS-1,:]=comm.recv(source=rank+1)
+
+print "Rank: %d\t M: %s " %(rank,M)
 
 #for elem in xrange(1,size-1):
 #	print elem
