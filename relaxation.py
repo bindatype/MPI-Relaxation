60ce1f8f52ee22b6861fa3fd8fd6cca674157054
Glen Alan MacLachlan <maclach@gwu.edu>
2017-10-18 10:07:26 -0400

    Changed to bcast to distribute grid, renamed message, and made M the primary data object.

--- relaxation.py
+++ relaxation.py
@@ -16,15 +16,19 @@ if size > ROWS:
 subROWS=ROWS//size+2
 #print subROWS
 
+M=None
 if rank == 0:
 	M=numpy.array(range(COLS*(ROWS+2))).reshape((ROWS+2, COLS)).astype('float')
 #	M=numpy.empty((ROWS+2, COLS)).astype('float')
 #	M=numpy.zeros((ROWS+2, COLS))
 	M[0,:] = 1.
 	M[:,0] = 1.
-#	M.astype('float')
-	for elem in M: 
-   		print elem
+
+M=comm.bcast(M,root=0)
+comm.barrier()
+#for elem in M: 
+#	print elem
+
 #if rank ==0:
 #	for elem in M[0:4,:]: # rank 0
 #		print "-",elem
@@ -38,25 +42,36 @@ if rank == 0:
 	# M[2*rank:2*rank+subROWS]
 
 #distribute initial grid
-message = None
-if rank == 0:
-	for destination in xrange(size):
-		message = M[2*destination:2*destination+subROWS,:]
-		comm.send(message,dest=destination)
-
-message = comm.recv(source=0,status = stat)
-sender = stat.Get_source()
-
-#for elem in message:
-print "Rank: %d\tSender: %d\tMessage:\n %s" % (rank,sender,message)
-
-for elem in xrange(1,COLS-1):
-	message[1,elem] = (message[1,elem-1]+message[1,elem+1]+message[0,elem]+message[2,elem])/4.
-	message[2,elem] = (message[2,elem-1]+message[2,elem+1]+message[1,elem]+message[3,elem])/4.
-#	print "Rank %d \t message[1,%d]=%f,message[2,%d]=%f" % (rank,elem,message[1,elem],elem,message[2,elem])
-print "New Rank: %d\tSender: %d\tMessage:\n %s" % (rank,sender,message)
-
-#M = comm.gather(message, root=0)
+subM = None
 #if rank == 0:
-#        for elem in M:
-#                print "+",elem
+#	for destination in xrange(size):
+#		subM = M[2*destination:2*destination+subROWS,:]
+#		comm.send(subM,dest=destination)
+
+#subM = comm.recv(source=0,status = stat)
+#sender = stat.Get_source()
+#for elem in subM:
+#print "Rank: %d\tSender: %d\tMessage:\n %s" % (rank,sender,subM)
+
+
+sender = -5
+subM = M[2*rank:2*rank+subROWS,:]
+for elem in xrange(1,COLS-1):
+	subM[1,elem] = (subM[1,elem-1]+subM[1,elem+1]+subM[0,elem]+subM[2,elem])/4.
+	subM[2,elem] = (subM[2,elem-1]+subM[2,elem+1]+subM[1,elem]+subM[3,elem])/4.
+#	print "Rank %d \t subM[1,%d]=%f,subM[2,%d]=%f" % (rank,elem,subM[1,elem],elem,subM[2,elem])
+#print "New Rank: %d\tSender: %d\tMessage:\n %s" % (rank,sender,subM)
+M[2*rank:2*rank+subROWS,:]=subM 
+
+if rank == 0:
+	print "Rank 0"
+	for elem in M:
+		print elem	
+elif rank == size-1:
+	print "Rank size-1"
+else: 
+	print "Else"
+
+
+#for elem in xrange(1,size-1):
+#	print elem
