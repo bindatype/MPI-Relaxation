7b3fcac7babaedface577321101426a0e0e0f343
Glen Alan MacLachlan <maclach@gwu.edu>
2017-10-18 00:26:12 -0400

    6th commit to added initial relaxation sweep.

--- relaxation.py
+++ relaxation.py
@@ -17,13 +17,14 @@ subROWS=ROWS//size+2
 #print subROWS
 
 if rank == 0:
-	M=numpy.array(range(COLS*(ROWS+2))).reshape((ROWS+2, COLS))
-#	M=numpy.empty((ROWS+2, COLS))
-	M[0,:] = 1
-	M[:,0] = 1
+	M=numpy.array(range(COLS*(ROWS+2))).reshape((ROWS+2, COLS)).astype('float')
+#	M=numpy.empty((ROWS+2, COLS)).astype('float')
+#	M=numpy.zeros((ROWS+2, COLS))
+	M[0,:] = 1.
+	M[:,0] = 1.
+#	M.astype('float')
 	for elem in M: 
    		print elem
-
 #if rank ==0:
 #	for elem in M[0:4,:]: # rank 0
 #		print "-",elem
@@ -47,4 +48,15 @@ message = comm.recv(source=0,status = stat)
 sender = stat.Get_source()
 
 #for elem in message:
-print "Rank: %d\tSender: %dMessage:\n %s" % (rank,sender,message)
+print "Rank: %d\tSender: %d\tMessage:\n %s" % (rank,sender,message)
+
+for elem in xrange(1,COLS-1):
+	message[1,elem] = (message[1,elem-1]+message[1,elem+1]+message[0,elem]+message[2,elem])/4.
+	message[2,elem] = (message[2,elem-1]+message[2,elem+1]+message[1,elem]+message[3,elem])/4.
+#	print "Rank %d \t message[1,%d]=%f,message[2,%d]=%f" % (rank,elem,message[1,elem],elem,message[2,elem])
+print "New Rank: %d\tSender: %d\tMessage:\n %s" % (rank,sender,message)
+
+#M = comm.gather(message, root=0)
+#if rank == 0:
+#        for elem in M:
+#                print "+",elem
