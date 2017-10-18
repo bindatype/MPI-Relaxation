df71ee020ceb8a781e986be62973d230b1c47a8e
Glen Alan MacLachlan <maclach@gwu.edu>
2017-10-18 13:18:12 -0400

    Not quite working out for -n 2 but -n 4 seems OK. Need to merge final grids into a single grid

--- relaxation.py
+++ relaxation.py
@@ -6,16 +6,15 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 stat = MPI.Status()
 
+
 COLS = 4
 ROWS = 8
-
-
 if size > ROWS:
 	print("Not enough ROWS")
 	exit()
 subROWS=ROWS//size+2
-#print subROWS
 
+# Set up initial grid on rank 0
 M=None
 if rank == 0:
 	M=numpy.array(range(COLS*(ROWS+2))).reshape((ROWS+2, COLS)).astype('float')
@@ -24,35 +23,35 @@ if rank == 0:
 	M[0,:] = 1.
 	M[:,0] = 1.
 
-#distribute initial grid
+#distribute initial grid to other ranks
 M=comm.bcast(M,root=0)
 comm.barrier()
 
-subM = None
+for i in xrange(10):
+	#parse out subgrids for each rank and sweep thru
+	subM = M[(ROWS/size)*rank:(ROWS/size)*rank+subROWS,:]
+	for subROW in xrange(1,subROWS-1):
+		for elem in xrange(1,COLS-1):
+			subM[subROW,elem] = (subM[subROW,elem-1]+subM[subROW,elem+1]+subM[subROW-1,elem]+subM[subROW+1,elem])/4.
+	M[2*rank:2*rank+subROWS,:]=subM 
+	#exhange edges for next interation
+	if rank == 0:
+		comm.send(M[ROWS*(rank+1)/size,:],dest=rank+1)
+		M[ROWS*(rank+1)/size+1,:]=comm.recv(source=rank+1)
+	elif rank == size-1:
+		comm.send(M[ROWS*rank/size+1,:],dest=rank-1)
+		M[ROWS*(rank)/size,:]=comm.recv(source=rank-1)
+	else: 
+		comm.send(M[ROWS*rank/size+1,:],dest=rank-1)
+		comm.send(M[ROWS*(rank+1)/size,:],dest=rank+1)
+		M[ROWS*(rank)/size,:]=comm.recv(source=rank-1)
+		M[ROWS*(rank+1)/size+1,:]=comm.recv(source=rank+1)
+	#print current status
+#	print "Rank:0 %d\t M: %s " %(rank,M)
 
-sender = -5
-subM = M[2*rank:2*rank+subROWS,:]
-for elem in xrange(1,COLS-1):
-	subM[1,elem] = (subM[1,elem-1]+subM[1,elem+1]+subM[0,elem]+subM[2,elem])/4.
-	subM[2,elem] = (subM[2,elem-1]+subM[2,elem+1]+subM[1,elem]+subM[3,elem])/4.
-#	print "Rank %d \t subM[1,%d]=%f,subM[2,%d]=%f" % (rank,elem,subM[1,elem],elem,subM[2,elem])
-#print "New Rank: %d\tSender: %d\tMessage:\n %s" % (rank,sender,subM)
-M[2*rank:2*rank+subROWS,:]=subM 
 
 
+subM = M[(ROWS/size)*rank:(ROWS/size)*rank+subROWS,:]
+newM=comm.gather(subM,root=0)
 if rank == 0:
-	comm.send(M[subROWS-2,:],dest=rank+1)
-	M[subROWS-1,:]=comm.recv(source=rank+1)
-elif rank == size-1:
-	comm.send(M[2*rank+1,:],dest=rank-1)
-	M[ROWS-subROWS,:]=comm.recv(source=rank-1)
-else: 
-	comm.send(M[rank*subROWS,:],dest=rank-1)
-	comm.send(M[(rank+0)*subROWS,:],dest=rank+1)
-	M[ROWS-subROWS,:]=comm.recv(source=rank-1)
-	M[2*rank+subROWS-1,:]=comm.recv(source=rank+1)
-
-print "Rank: %d\t M: %s " %(rank,M)
-
-#for elem in xrange(1,size-1):
-#	print elem
+	print "Rank:0 %d\t M: %s " %(rank,newM)
