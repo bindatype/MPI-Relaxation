271a65cba228c675cbdcf80fb236a048358ea03b
Glen Alan MacLachlan <maclach@gwu.edu>
2017-11-12 11:04:20 -0500

    Changed M to Grid to match lecture.

--- relaxation.py
+++ relaxation.py
@@ -15,45 +15,45 @@ if size > ROWS:
 subROWS=ROWS//size+2
 
 # Set up initial grid on rank 0
-M=None
+Grid=None
 if rank == 0:
 #	M=numpy.array(range(COLS*(ROWS+2))).reshape((ROWS+2, COLS)).astype('float')
 #	M=numpy.empty((ROWS+2, COLS)).astype('float')
-	M=numpy.zeros((ROWS+2, COLS))
-	M[0,:] = 1.
-	M[:,0] = 1.
-	initM = M
+	Grid=numpy.zeros((ROWS+2, COLS))
+	Grid[0,:] = 1.
+	Grid[:,0] = 1.
+	initGrid = Grid
 
 #distribute initial grid to other ranks
-M=comm.bcast(M,root=0)
+Grid=comm.bcast(Grid,root=0)
 
 for i in xrange(100):
 	#parse out subgrids for each rank and sweep thru
-	subM = M[(ROWS/size)*rank:(ROWS/size)*rank+subROWS,:]
+	subGrid = Grid[(ROWS/size)*rank:(ROWS/size)*rank+subROWS,:]
 	for subROW in xrange(1,subROWS-1):
 		for elem in xrange(1,COLS-1):
-			subM[subROW,elem] = (subM[subROW,elem-1]
-						+subM[subROW,elem+1]
-						+subM[subROW-1,elem]
-						+subM[subROW+1,elem])/4.
-	M[2*rank:2*rank+subROWS,:]=subM 
+			subGrid[subROW,elem] = (subGrid[subROW,elem-1]
+						+subGrid[subROW,elem+1]
+						+subGrid[subROW-1,elem]
+						+subGrid[subROW+1,elem])/4.
+	Grid[2*rank:2*rank+subROWS,:]=subGrid 
 	#exhange edges for next interation
 	if rank == 0:
-		comm.send(M[ROWS*(rank+1)/size,:],dest=rank+1)
-		M[ROWS*(rank+1)/size+1,:]=comm.recv(source=rank+1)
+		comm.send(Grid[ROWS*(rank+1)/size,:],dest=rank+1)
+		Grid[ROWS*(rank+1)/size+1,:]=comm.recv(source=rank+1)
 	elif rank == size-1:
-		comm.send(M[ROWS*rank/size+1,:],dest=rank-1)
-		M[ROWS*(rank)/size,:]=comm.recv(source=rank-1)
+		comm.send(Grid[ROWS*rank/size+1,:],dest=rank-1)
+		Grid[ROWS*(rank)/size,:]=comm.recv(source=rank-1)
 	else: 
-		comm.send(M[ROWS*rank/size+1,:],dest=rank-1)
-		comm.send(M[ROWS*(rank+1)/size,:],dest=rank+1)
-		M[ROWS*(rank)/size,:]=comm.recv(source=rank-1)
-		M[ROWS*(rank+1)/size+1,:]=comm.recv(source=rank+1)
+		comm.send(Grid[ROWS*rank/size+1,:],dest=rank-1)
+		comm.send(Grid[ROWS*(rank+1)/size,:],dest=rank+1)
+		Grid[ROWS*(rank)/size,:]=comm.recv(source=rank-1)
+		Grid[ROWS*(rank+1)/size+1,:]=comm.recv(source=rank+1)
 
 
-subM = M[(ROWS/size)*rank+1:(ROWS/size)*rank+subROWS-1,:]
-newM=comm.gather(subM,root=0)
+subGrid = Grid[(ROWS/size)*rank+1:(ROWS/size)*rank+subROWS-1,:]
+newGrid=comm.gather(subGrid,root=0)
 if rank == 0:
-	result= numpy.vstack(newM)
-	print numpy.vstack(initM)
+	result= numpy.vstack(newGrid)
+	print numpy.vstack(initGrid)
 	print result
