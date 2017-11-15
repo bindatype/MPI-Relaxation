36e982bf9abecd2a8519acf964ddac7614c10aec
Glen Alan MacLachlan <maclach@gwu.edu>
2017-11-15 17:29:04 -0500

    Each worker now computes initial subgrid. No global grid is passed around.

--- relaxation.py
+++ relaxation.py
@@ -1,3 +1,4 @@
+#from memory_profiler import profile
 from mpi4py import MPI
 import numpy
 
@@ -6,6 +7,7 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 stat = MPI.Status()
 
+# Set global variables
 COLS = 100
 ROWS = 8192 
 if size > ROWS:
@@ -13,12 +15,16 @@ if size > ROWS:
         exit()
 subROWS=ROWS//size+2
 
+# Function definitions 
+#@profile
 def msgUp(subGrid):
+	# Sends and Recvs rows with Rank+1
         comm.send(subGrid[subROWS-2,:],dest=rank+1)
         subGrid[subROWS-1,:]=comm.recv(source=rank+1)
         return 0
 
 def msgDn(subGrid):
+	# Sends and Recvs rows with Rank-1
         comm.send(subGrid[1,:],dest=rank-1)
         subGrid[0,:] = comm.recv(source=rank-1)
         return 0
@@ -26,36 +32,27 @@ def msgDn(subGrid):
 def computeGridPoints(subGrid):
 	for subROW in xrange(1,subROWS-1):
 		for elem in xrange(1,COLS-1):
-			subGrid[subROW,elem] = (subGrid[subROW,elem-1]
+			subGrid[subROW,elem] = (
+			subGrid[subROW,elem-1]
 			+subGrid[subROW,elem+1]
 			+subGrid[subROW-1,elem]
 			+subGrid[subROW+1,elem])/4.
 	return 0
 
+# All workers initialize a zero-valued subgrid and 
+# boundary conditions are assigned by rank as below. 
+# This minimizes memory and communication compared to 
+# bcast or scatter.
+subGrid = numpy.zeros((subROWS, COLS))
 
-# Set up initial grid on rank 0
-Grid=None
-#if rank == 0:
-##	M=numpy.array(range(COLS*(ROWS+2))).reshape((ROWS+2, COLS)).astype('float')
-##	M=numpy.empty((ROWS+2, COLS)).astype('float')
-#	Grid=numpy.zeros((ROWS+2, COLS))
-#	Grid[0,:] = 1.
-#	Grid[:,0] = 1.
-#	initGrid = Grid
+# BC for all ranks.
+subGrid[:,0] = 1.
 
+# BC for rank 0.
+if rank ==0:
+	subGrid[0,:] = 1
 
-#Every worker computes own grid.
-Grid=numpy.zeros((ROWS+2, COLS))
-Grid[0,:] = 1.
-Grid[:,0] = 1.
-
-#distribute initial grid to other ranks
-#Grid=comm.bcast(Grid,root=0)
-
-#parse out subgrids for each rank and delete Grid
-subGrid = numpy.copy(Grid[(ROWS/size)*rank:(ROWS/size)*rank+subROWS,:])
-del Grid
-
+# The main body of the algorithm
 #compute new grid and pass rows to neighbors
 for i in xrange(100):
 	computeGridPoints(subGrid)
@@ -74,5 +71,5 @@ newGrid=comm.gather(subGrid[1:subROWS-1,:],root=0)
 if rank == 0:
 	result= numpy.vstack(newGrid)
 #	print numpy.vstack(initGrid)
-	print result[0]
+	print result[-1]
 
